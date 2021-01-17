cfd6ff3c53dbbd7aa394502a17939f1ccb41568c
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-01-17 17:55:37 +0545

    Delivers found cache values on non-empty cache

--- FeedStoreChallenge/CoreDataFeedStrore.swift
+++ FeedStoreChallenge/CoreDataFeedStrore.swift
@@ -40,10 +40,48 @@ public class CoreDataFeedStore: FeedStore {
 	}
 	
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		
+		let context = self.context
+		context.perform {
+			do {
+				let cache = CDCache(context: context)
+				cache.timeStamp = timestamp
+				cache.feed = NSSet(array: feed.map { local in
+					let feed = CDFeedImage(context: context)
+					feed.id = local.id
+					feed.imageDescription = local.description
+					feed.location = local.location
+					feed.url = local.url
+					return feed
+				})
+				
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+		}
 	}
 	
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			do {
+				let request = NSFetchRequest<CDCache>(entityName: CDCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				if let cache = try context.fetch(request).first {
+					completion(.found(
+								feed: cache.feed
+									.compactMap { ($0 as? CDFeedImage) }
+									.map {
+										LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+									},
+								timestamp: cache.timeStamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 }
