3e7af4ef78a93e6ee98c16b12ae5c22bd915144e
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-01-18 18:51:48 +0545

    Remove unwanted dependency with Foundation

--- FeedStoreChallenge/CDCache.swift
+++ FeedStoreChallenge/CDCache.swift
@@ -7,7 +7,6 @@
 //
 //
 
-import Foundation
 import CoreData
 
 public class CDCache: NSManagedObject {
--- FeedStoreChallenge/CDFeedImage.swift
+++ FeedStoreChallenge/CDFeedImage.swift
@@ -7,7 +7,6 @@
 //
 //
 
-import Foundation
 import CoreData
 
 public class CDFeedImage: NSManagedObject {
